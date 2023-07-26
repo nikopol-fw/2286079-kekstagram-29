4f9b2148f0ab63f77ca4d477947e24247f8cb9a3
Наталья Моисеева <mns-fw@bk.ru>
2023-07-26 22:08:07 +0300

    Добавляет и подключает модуль сортировки фото

--- js/main.js
+++ js/main.js
@@ -1,14 +1,17 @@
 import { renderPictures } from './pictures.js';
 import { closeForm} from './form.js';
-import { getData, sendData } from './api.js';
+import { getData, sendData } from './network.js';
 import { showAlert } from './decor-message.js';
 import { setFormSubmit } from './form.js';
 import { showErrorMessage, showSuccessMessage } from './send-message.js';
+import { initFilter, showFilters } from './sort.js';
 
 /** Получаем данные с сервера */
 try {
   const data = await getData();
   renderPictures(data);
+  showFilters();
+  initFilter(data);
 } catch (err) {
   showAlert(err.message);
 }
